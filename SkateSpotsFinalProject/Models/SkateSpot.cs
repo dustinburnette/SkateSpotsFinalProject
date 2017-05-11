eaa5e86f196bc1fae9fba540575045bc42259491
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-05-11 11:19:17 -0500

    Added Model for skate spots page

---
+++ SkateSpotsFinalProject/Models/SkateSpot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkateSpotsFinalProject.Models
+{
+    public class SkateSpot
+    {
+        public int SkateSpotID { get; set; }
+        public string Description { get; set; }
+        public int Lat { get; set; }
+        public int Long { get; set; }
+
+    }
+}
